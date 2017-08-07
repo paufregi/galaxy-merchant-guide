b6ae8148ca2229787a87edf98c16c31c907e2fca
PauFregi <paulfrancis.ellis@gmail.com>
2017-08-07 10:56:14 +0100

    Galaxy Guide

---
+++ src/main/scala/pellis/GalaxyGuide.scala
@@ -0,0 +1,92 @@
+package pellis
+
+import scala.util.{Failure, Success, Try}
+
+object GalaxyGuide {
+
+  private val ERROR_MESSAGE: String = "I have no idea what you are talking about"
+
+  object RegEx {
+    val galaxyConversion = """^([a-zA-Z]+) is ([A-Z])$"""
+    val exchangeRate = """^([a-zA-Z_ ]+) ([a-zA-Z]+) is (\d+) [c,C]redits$"""
+    val howMuch = """^[H,h]ow much is ([a-z_ ]+) \?$"""
+    val howManyCredits = """^[H,h]ow many [c,C]redits is ([a-zA-Z_ ]+) ([a-zA-Z_ ]+) \?$"""
+  }
+
+  private case class Execution(output: List[String], exchangeRate: ExchangeRate, cypher: GalaxyCypher)
+
+  private object Execution {
+    def empty: Execution = Execution(List.empty, Map.empty, Map.empty)
+  }
+
+  def compute(input: List[String]): List[String] = {
+    val execution: Execution = input.foldLeft(Execution.empty) {
+      (execution, line) => execute(execution, line)
+    }
+
+    execution.output
+  }
+
+  private def execute(execution: Execution, input: String): Execution = input match {
+    case line if line.matches(RegEx.galaxyConversion) =>
+      execution.copy(cypher = learnRoman(execution.cypher, line))
+    case line if line.matches(RegEx.exchangeRate) =>
+      execution.copy(exchangeRate = learnExchangeRate(execution.exchangeRate, execution.cypher, line))
+    case line if line.matches(RegEx.howMuch) =>
+      execution.copy(output = execution.output :+ answerHowMuch(execution.cypher, line))
+    case line if line.matches(RegEx.howManyCredits) =>
+      execution.copy(output = execution.output :+ answerHowManyCredits(execution.exchangeRate, execution.cypher, line))
+    case line if line.isEmpty | line.trim.length == 0 => execution
+    case _ => execution.copy(output = execution.output :+ ERROR_MESSAGE)
+  }
+
+  def learnRoman(cypher: GalaxyCypher, input: String): GalaxyCypher = {
+    val pattern = RegEx.galaxyConversion.r
+    input match {
+      case pattern(galaxyNumber, romanNumber) if RomanNumber.validate(romanNumber) =>
+        cypher.filterNot { case (_, value) => value == romanNumber } + (galaxyNumber.toLowerCase -> romanNumber)
+      case _ => cypher
+    }
+  }
+
+  def learnExchangeRate(exchangeRate: ExchangeRate, cypher: GalaxyCypher, input: String): ExchangeRate = {
+    val pattern = RegEx.exchangeRate.r
+    input match {
+      case pattern(galaxyNumber, currency, value) =>
+        Try(GalaxyNumber.toInt(galaxyNumber.toLowerCase, cypher)) match {
+          case Success(number) =>
+            val unitValue = value.toDouble / number
+            exchangeRate + (currency.toLowerCase -> unitValue)
+          case Failure(_) => exchangeRate
+        }
+      case _ => exchangeRate
+    }
+  }
+
+  def answerHowMuch(cypher: GalaxyCypher, input: String): String = {
+    val pattern = RegEx.howMuch.r
+    input match {
+      case pattern(galaxyNumber) =>
+        Try(GalaxyNumber.toInt(galaxyNumber.toLowerCase, cypher)) match {
+          case Success(number) => s"$galaxyNumber is $number"
+          case Failure(_) => s"Sorry, $galaxyNumber is not a number"
+        }
+      case _ => ERROR_MESSAGE
+    }
+  }
+
+  def answerHowManyCredits(exchangeRate: ExchangeRate, cypher: GalaxyCypher, input: String): String = {
+    val pattern = RegEx.howManyCredits.r
+    input match {
+      case pattern(galaxyNumber, currency) =>
+        (for {
+          unitValue <- exchangeRate.get(currency.toLowerCase)
+          number <- Try(GalaxyNumber.toInt(galaxyNumber.toLowerCase, cypher)).toOption
+        } yield f"$galaxyNumber $currency is ${unitValue * number}%.0f Credits") match {
+          case Some(answer) => answer
+          case None => s"Sorry, can't convert $galaxyNumber $currency in Credits"
+        }
+      case _ => ERROR_MESSAGE
+    }
+  }
+}
---
+++ src/test/scala/pellis/GalaxyGuideTest.scala
@@ -0,0 +1,214 @@
+package pellis
+
+import org.scalatest.{Matchers, WordSpec}
+
+class GalaxyGuideTest extends WordSpec with Matchers {
+
+  "#compute" should {
+    "analyse the input and answer accordingly" in {
+      val input = List(
+        "glob is I",
+        "prok is V",
+        "pish is X",
+        "tegj is L",
+        "glob glob Silver is 34 Credits",
+        "glob prok Gold is 57800 Credits",
+        "pish pish Iron is 3910 Credits",
+        "how much is pish tegj glob glob ?",
+        "how many Credits is glob prok Silver ?",
+        "how many Credits is glob prok Gold ?",
+        "how many Credits is glob prok Iron ?",
+        "how much wood could a woodchuck chuck if a woodchuck could chuck wood ?")
+
+      val expected = List(
+        "pish tegj glob glob is 42",
+        "glob prok Silver is 68 Credits",
+        "glob prok Gold is 57800 Credits",
+        "glob prok Iron is 782 Credits",
+        "I have no idea what you are talking about"
+      )
+
+      GalaxyGuide.compute(input) shouldBe expected
+    }
+
+    "skip empty lines" in {
+      val input = List(
+        "glob is I",
+        "prok is V",
+        "pish is X",
+        "",
+        "tegj is L",
+        "glob glob Silver is 34 Credits",
+        "glob prok Gold is 57800 Credits",
+        "pish pish Iron is 3910 Credits",
+        " ",
+        "how much is pish tegj glob glob ?",
+        "how many Credits is glob prok Silver ?",
+        "how many Credits is glob prok Gold ?",
+        "\t",
+        "how many Credits is glob prok Iron ?",
+        "how much wood could a woodchuck chuck if a woodchuck could chuck wood ?")
+
+      val expected = List(
+        "pish tegj glob glob is 42",
+        "glob prok Silver is 68 Credits",
+        "glob prok Gold is 57800 Credits",
+        "glob prok Iron is 782 Credits",
+        "I have no idea what you are talking about"
+      )
+
+      GalaxyGuide.compute(input) shouldBe expected
+    }
+  }
+
+  "#learnGalaxyConversion" should {
+    "return the cypher with a new entry" in {
+      val cypher = Map("glob" -> "I", "prok" -> "V")
+      val input: String = "pish is X"
+
+      val expected = Map("glob" -> "I", "prok" -> "V", "pish" -> "X")
+
+      GalaxyGuide.learnRoman(cypher, input) shouldBe expected
+    }
+
+    "override conversion with the newest version" in {
+      val cypher = Map("glob" -> "I", "prok" -> "V", "pish" -> "X")
+      val input: String = "prok is I"
+
+      val expected = Map("prok" -> "I", "pish" -> "X")
+
+      GalaxyGuide.learnRoman(cypher, input) shouldBe expected
+    }
+
+    "return the cypher untouched if the input is invalid" in {
+      val cypher = Map("glob" -> "I", "prok" -> "V")
+      val input: String = "invalid input"
+
+      val expected = Map("glob" -> "I", "prok" -> "V")
+
+      GalaxyGuide.learnRoman(cypher, input) shouldBe expected
+    }
+
+    "return the cypher untouched if the roman number is invalid" in {
+      val cypher = Map("glob" -> "I", "prok" -> "V")
+      val input: String = "pish is A"
+
+      val expected = Map("glob" -> "I", "prok" -> "V")
+
+      GalaxyGuide.learnRoman(cypher, input) shouldBe expected
+    }
+  }
+
+  "#learnExchangeRate" should {
+    "return the exchange rate collection with a new entry" in {
+      val exchangeRate = Map("silver" -> 17d, "gold" -> 14450d)
+      val cypher = Map("glob" -> "I", "prok" -> "V", "pish" -> "X", "tegj" -> "L")
+
+      val input = "pish pish Iron is 3910 Credits"
+      val expected = Map("silver" -> 17d, "gold" -> 14450d, "iron" -> 195.5)
+
+      GalaxyGuide.learnExchangeRate(exchangeRate, cypher, input) shouldBe expected
+    }
+
+    "override exchange rate whit the newest version" in {
+      val exchangeRate = Map("silver" -> 17d, "gold" -> 14450d)
+      val cypher = Map("glob" -> "I", "prok" -> "V", "pish" -> "X", "tegj" -> "L")
+
+      val input = "pish pish Gold is 3910 Credits"
+      val expected = Map("silver" -> 17d, "gold" -> 195.5)
+
+      GalaxyGuide.learnExchangeRate(exchangeRate, cypher, input) shouldBe expected
+    }
+
+    "return the cypher untouched if the input is invalid" in {
+      val exchangeRate = Map("silver" -> 17d, "gold" -> 14450d)
+      val cypher = Map("glob" -> "I", "prok" -> "V", "pish" -> "X", "tegj" -> "L")
+
+      val input = "invalid input"
+      val expected = Map("silver" -> 17d, "gold" -> 14450d)
+
+      GalaxyGuide.learnExchangeRate(exchangeRate, cypher, input) shouldBe expected
+    }
+
+    "return the cypher untouched if the number is invalid" in {
+      val exchangeRate = Map("silver" -> 17d, "gold" -> 14450d)
+      val cypher = Map("glob" -> "I", "prok" -> "V", "pish" -> "X", "tegj" -> "L")
+
+      val input = "pish glob tegj Gold is 3910 Credits"
+      val expected = Map("silver" -> 17d, "gold" -> 14450d)
+
+      GalaxyGuide.learnExchangeRate(exchangeRate, cypher, input) shouldBe expected
+    }
+  }
+
+  "#answerHowMuch" should {
+    "return the correct answer" in {
+      val cypher = Map("glob" -> "I", "prok" -> "V", "pish" -> "X", "tegj" -> "L")
+
+      val input = "how much is pish tegj glob glob ?"
+      val expected = "pish tegj glob glob is 42"
+
+      GalaxyGuide.answerHowMuch(cypher, input) shouldBe expected
+    }
+
+    "return an error message if the input is invalid" in {
+      val cypher = Map("glob" -> "I", "prok" -> "V", "pish" -> "X", "tegj" -> "L")
+
+      val input = "invalid input ?"
+      val expected = "I have no idea what you are talking about"
+
+      GalaxyGuide.answerHowMuch(cypher, input) shouldBe expected
+    }
+
+    "return an error message if the number is invalid" in {
+      val cypher = Map("glob" -> "I", "prok" -> "V", "pish" -> "X", "tegj" -> "L")
+
+      val input = "how much is tegj pish tegj glob glob ?"
+      val expected = "Sorry, tegj pish tegj glob glob is not a number"
+
+      GalaxyGuide.answerHowMuch(cypher, input) shouldBe expected
+    }
+  }
+
+  "#answerHowManyCredits" should {
+    "return the correct answer" in {
+      val exchangeRate = Map("silver" -> 17d, "gold" -> 14450d, "iron" -> 195.5)
+      val cypher = Map("glob" -> "I", "prok" -> "V", "pish" -> "X", "tegj" -> "L")
+
+      val input = "how many Credits is glob prok Silver ?"
+      val expected = "glob prok Silver is 68 Credits"
+
+      GalaxyGuide.answerHowManyCredits(exchangeRate, cypher, input) shouldBe expected
+    }
+
+    "return an error message if the input is invalid" in {
+      val exchangeRate = Map("silver" -> 17d, "gold" -> 14450d, "iron" -> 195.5)
+      val cypher = Map("glob" -> "I", "prok" -> "V", "pish" -> "X", "tegj" -> "L")
+
+      val input = "invalid input ?"
+      val expected = "I have no idea what you are talking about"
+
+      GalaxyGuide.answerHowManyCredits(exchangeRate, cypher, input) shouldBe expected
+    }
+
+    "return an error message if the currency is invalid" in {
+      val exchangeRate = Map("silver" -> 17d, "gold" -> 14450d, "iron" -> 195.5)
+      val cypher = Map("glob" -> "I", "prok" -> "V", "pish" -> "X", "tegj" -> "L")
+
+      val input = "how many Credits is glob prok Jade ?"
+      val expected = "Sorry, can't convert glob prok Jade in Credits"
+
+      GalaxyGuide.answerHowManyCredits(exchangeRate, cypher, input) shouldBe expected
+    }
+
+    "return an error message if the number is invalid" in {
+      val exchangeRate = Map("silver" -> 17d, "gold" -> 14450d, "iron" -> 195.5)
+      val cypher = Map("glob" -> "I", "prok" -> "V", "pish" -> "X", "tegj" -> "L")
+
+      val input = "how many Credits is glob prok tegj Silver ?"
+      val expected = "Sorry, can't convert glob prok tegj Silver in Credits"
+
+      GalaxyGuide.answerHowManyCredits(exchangeRate, cypher, input) shouldBe expected
+    }
+  }
+}
